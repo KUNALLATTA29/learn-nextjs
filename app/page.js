2020c3c35a5290479e319082d771be434ded20b9
KUNALLATTA29 <kunallatta2@gmail.com>
2025-01-21 20:50:35 +0530

    use client

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
-import React from 'react'
+"use client"
+
+import React,{useState} from 'react'
 
 function page() {
+  const [count,setcount] = useState(0)
+  
   return (
     <div>
       <h1>my home page</h1>
-      <h3>learning routing</h3>
+      <h3>{count}</h3>
+      <button onClick={()=>setcount(count+1)}>+</button>
     </div>
   )
 }
